5f073ecf9df437331ed2e64eea735cb0b05013bd
Abdul Rehman <161899946+Ab-Rehman01@users.noreply.github.com>
2025-01-07 06:21:37 +0500

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,32 +1,16 @@
-import { Metadata } from 'next'
-import { Suspense } from 'react'
-//import Loader from '@/components/Loader'
-import pro-gride from '@/components/productgrid/proGride'
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+import { ProGrid } from '@/components/productsgrid/proGrid';
 
 export const metadata: Metadata = {
   title: 'Our Products | SHOP.CO',
   description: 'Explore our wide range of stylish clothing and accessories. Find the perfect outfit that matches your unique style at SHOP.CO.',
-  openGraph: {
-    title: 'Our Products | SHOP.CO',
-    description: 'Explore our wide range of stylish clothing and accessories. Find the perfect outfit that matches your unique style.',
-    type: 'website',
-    url: 'https://www.shopco-.vercel.app/products',
-    images: [
-      {
-        url: '/images/products-banner.jpg',
-        width: 1200,
-        height: 630,
-        alt: 'SHOP.CO Products',
-      },
-    ],
-  },
-}
+};
 
 export default function Products() {
   return (
-      <Suspense fallback={<div>{<Loader />}</div>}>
-        <proGride />
-      </Suspense>
- 
-  )
+    <Suspense fallback={<div>Loading...</div>}>
+      <ProGrid />
+    </Suspense>
+  );
 }
